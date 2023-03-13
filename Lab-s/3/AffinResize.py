d1f2cfe8bc04a221f4fc947995de69b57fc0b58d
NemoNology <chicken.gang@mail.ru>
2023-03-14 05:12:21 +0900

    Build 1.0

--- Lab-s/3/AffinResize.py
+++ Lab-s/3/AffinResize.py
@@ -2,42 +2,45 @@ import numpy as np
 
 def PrintMatrixFloat(matrix):
 
-    for i in range(0, matrix.shape[0]):
+    for y in range(0, matrix.shape[0]):
 
-        for j in range(0, matrix.shape[1]):
+        for x in range(0, matrix.shape[1]):
 
-            print(f"( {matrix[i][j][0]:.3f}, {matrix[i][j][1]:.3f} )", end='')
+            print(f"( {matrix[y][x][0]:4.1f}, {matrix[y][x][1]:4.1f} )", end='')
 
         print()
 
 def PrintMatrixInt(matrix):
 
-    for i in range(0, matrix.shape[0]):
+    for y in range(0, matrix.shape[0]):
 
-        for j in range(0, matrix.shape[1]):
+        for x in range(0, matrix.shape[1]):
 
-            print(f"( {matrix[i][j][0]}, {matrix[i][j][1]} )", end='')
+            print(f"( {matrix[y][x][0]:2}, {matrix[y][x][1]:2} )", end='')
 
         print()
 
 ###############################################################################
 
-mX = int(input("Input matrix Height: "))
-mY = int(input("Input matrix Width: "))
+# mY = int(input("Input matrix Height: "))
+# mX = int(input("Input matrix Width: "))
 
-im = np.array(np.zeros((mX, mY, 2)), int)
+mY, mX = 3, 7
 
-for i in range(0, mX):
+im = np.array(np.zeros((mY, mX, 2)), int)
 
-    for j in range(0, mY):
+for y in range(0, mY):
 
-        im[i][j] = [i + 1, j + 1]
+    for x in range(0, mX):
+
+        im[y][x] = [x + 1, y + 1]
 
 print(); print('Base Matrix:'); PrintMatrixInt(im); print();
 
 ###############################################################################
 
-if (bool(input('Calculate Resized Matrix? (Enter - NO, Another - YES): '))):
+# if (bool(input('Calculate Resized Matrix? (Enter - NO, Another - YES): '))):
+if (False):
 
     rx = float(input("Input resize Width coefficient (Base Matrix Width *= coefficient): "))
     ry = float(input("Input resize Height coefficient (Base Matrix Height *= coefficient): "))
@@ -46,9 +49,9 @@ if (bool(input('Calculate Resized Matrix? (Enter - NO, Another - YES): '))):
 
     imResized = np.array(im)
 
-    for i in range(0, mX):
+    for y in range(0, mX):
 
-        imResized[i] = np.matmul(im[i], resize)
+        imResized[y] = np.matmul(im[y], resize)
 
     print(); PrintMatrixInt(imResized)
 
@@ -59,7 +62,8 @@ imRotated = []
 
 print('Input Rotation Point Coordinates (Default is Matrix Center)? (Enter - NO, Another - YES): ', end='')
 
-if (bool(input())):
+# if (bool(input())):
+if (False):
 
     rpx = int((input("Input Rotation Point X coordinate: ")))
     rpy = int((input("Input Rotation Point Y coordinate: ")))
@@ -71,13 +75,13 @@ else:
 
 print(f"Rotation Point is ( {rpx}, {rpy} )")
 
-rotatePoint = (rpx, rpy);
+rotatePoint = [rpx, rpy];
 
-angle = np.radians(int(input("Input Rotation Angle in degrees: ")));
+# angle = np.radians(int(input("Input Rotation Angle in degrees: ")));
+
+angle = np.radians(-45)
 
 minX, minY, maxX, maxY = 1, 1, 1, 1;
-
-
 
 ###############################################################################
 
@@ -85,17 +89,17 @@ ccos = np.cos(angle)
 csin = np.sin(angle)
 
 
-for i in range(0, mX):
+for y in range(0, mY):
 
-    for j in range(0, mY):
+    for x in range(0, mX):
 
         # Info taken from https://ip76.ru/short-stories/rect-no-center-rotate/
 
         # O (x0, y0) - Rotation Center Point 
         # x' = (x - x0) * cos(alpha) - (y - y0) * sin(alpha) + x0
         # y' = (x - x0) * sin(alpha) + (y - y0) * cos(alpha) + y0
-        X = (im[i][j][0] - rotatePoint[0]) * ccos - (im[i][j][1] - rotatePoint[1]) * csin + rotatePoint[0]
-        Y = (im[i][j][0] - rotatePoint[0]) * csin + (im[i][j][1] - rotatePoint[1]) * ccos + rotatePoint[1]
+        X = (im[y][x][0] - rotatePoint[0]) * ccos - (im[y][x][1] - rotatePoint[1]) * csin + rotatePoint[0]
+        Y = (im[y][x][0] - rotatePoint[0]) * csin + (im[y][x][1] - rotatePoint[1]) * ccos + rotatePoint[1]
 
         if X < minX: minX = X
         if Y < minY: minY = Y
@@ -108,14 +112,19 @@ for i in range(0, mX):
 
 ###############################################################################
 
-print(); print(f"Base Matrix size: {mX} x {mY}"); PrintMatrixInt(im)
+print(); print(f"Base Matrix size: {mY} x {mX}"); PrintMatrixInt(im)
 
-imRotated = np.reshape(np.array(imRotated, float), (mX, mY, 2))
+buff1 = imRotated[-1]
+buff2 = imRotated[0]
+
+newSizeX = int(np.round(buff1[0] - buff2[0]) + 1)
+newSizeY = int(np.round(buff1[1] - buff2[1]) + 1)
+
+imRotated = np.reshape(np.array(imRotated, float), (newSizeY, newSizeX, 2))
 
 print(); print(f"Rotation angle: {np.degrees(angle)} degrees") 
-print(f"Rotated Matrix size: {int(maxX + minX)} x {int(maxX + minY)} - NOT TRUE!"); PrintMatrixFloat(imRotated)
+print(f"Rotated Matrix size: {int(newSizeY)} x {int(newSizeX)}"); PrintMatrixFloat(imRotated)
 
 
 print(); print(f'MinX: {minX:.3f}, MinY: {minY:.3f}')
 print(f'MaxX: {maxX:.3f}, MaxY: {maxY:.3f}')
-print(f'New size: {int(mX - minX) + 1} x {int(mY - minY) + 1}')
