04f099b50942e2e9fbba3a467d9af9e50914dddd
NemoNology <chicken.gang@mail.ru>
2023-03-21 08:35:31 +0900

    Added Cube draw possibility

--- Lab-s/4/Solution/WF/Form1.Designer.cs
+++ Lab-s/4/Solution/WF/Form1.Designer.cs
@@ -28,36 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
+            _mainView = new PictureBox();
             splitContainer1 = new SplitContainer();
-            label1 = new Label();
-            label2 = new Label();
-            inputCx = new NumericUpDown();
-            label3 = new Label();
-            inputCy = new NumericUpDown();
-            label4 = new Label();
+            buttonDraw = new Button();
+            inputKz = new NumericUpDown();
             inputCz = new NumericUpDown();
-            label5 = new Label();
+            label9 = new Label();
+            label4 = new Label();
             inputCubeSize = new NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            inputCy = new NumericUpDown();
+            label3 = new Label();
+            inputCx = new NumericUpDown();
+            label2 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)_mainView).BeginInit();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)inputCx).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)inputCy).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputKz).BeginInit();
             ((System.ComponentModel.ISupportInitialize)inputCz).BeginInit();
             ((System.ComponentModel.ISupportInitialize)inputCubeSize).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputCy).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputCx).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox1
+            // _mainView
             // 
-            pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(800, 288);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            _mainView.Dock = DockStyle.Fill;
+            _mainView.Location = new Point(0, 0);
+            _mainView.Name = "_mainView";
+            _mainView.Size = new Size(800, 288);
+            _mainView.TabIndex = 0;
+            _mainView.TabStop = false;
+            _mainView.SizeChanged += MainView_SizeChanged;
             // 
             // splitContainer1
             // 
@@ -69,12 +75,15 @@
             // splitContainer1.Panel1
             // 
             splitContainer1.Panel1.BackColor = SystemColors.Control;
-            splitContainer1.Panel1.Controls.Add(pictureBox1);
+            splitContainer1.Panel1.Controls.Add(_mainView);
             // 
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.BackColor = SystemColors.ActiveBorder;
+            splitContainer1.Panel2.Controls.Add(buttonDraw);
+            splitContainer1.Panel2.Controls.Add(inputKz);
             splitContainer1.Panel2.Controls.Add(inputCz);
+            splitContainer1.Panel2.Controls.Add(label9);
             splitContainer1.Panel2.Controls.Add(label4);
             splitContainer1.Panel2.Controls.Add(inputCubeSize);
             splitContainer1.Panel2.Controls.Add(inputCy);
@@ -82,53 +91,49 @@
             splitContainer1.Panel2.Controls.Add(inputCx);
             splitContainer1.Panel2.Controls.Add(label2);
             splitContainer1.Panel2.Controls.Add(label5);
+            splitContainer1.Panel2.Controls.Add(label6);
             splitContainer1.Panel2.Controls.Add(label1);
             splitContainer1.Size = new Size(800, 450);
             splitContainer1.SplitterDistance = 288;
             splitContainer1.SplitterWidth = 10;
             splitContainer1.TabIndex = 1;
             // 
-            // label1
+            // buttonDraw
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(12, 12);
-            label1.Name = "label1";
-            label1.Size = new Size(147, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Cube Center coordinates:";
+            buttonDraw.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonDraw.Location = new Point(419, 72);
+            buttonDraw.Name = "buttonDraw";
+            buttonDraw.Size = new Size(75, 23);
+            buttonDraw.TabIndex = 3;
+            buttonDraw.Text = "Draw";
+            buttonDraw.UseVisualStyleBackColor = true;
+            buttonDraw.Click += ButtonDraw_Click;
             // 
-            // label2
+            // inputKz
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(28, 39);
-            label2.Name = "label2";
-            label2.Size = new Size(17, 15);
-            label2.TabIndex = 1;
-            label2.Text = "X:";
+            inputKz.Location = new Point(683, 122);
+            inputKz.Maximum = new decimal(new int[] { 65536, 0, 0, 0 });
+            inputKz.Name = "inputKz";
+            inputKz.Size = new Size(83, 23);
+            inputKz.TabIndex = 2;
+            inputKz.Value = new decimal(new int[] { 100, 0, 0, 0 });
+            inputKz.ValueChanged += Kz_ValueChanged;
             // 
-            // inputCx
+            // inputCz
             // 
-            inputCx.Location = new Point(51, 37);
-            inputCx.Name = "inputCx";
-            inputCx.Size = new Size(83, 23);
-            inputCx.TabIndex = 2;
+            inputCz.Location = new Point(51, 116);
+            inputCz.Name = "inputCz";
+            inputCz.Size = new Size(83, 23);
+            inputCz.TabIndex = 2;
             // 
-            // label3
+            // label9
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(28, 80);
-            label3.Name = "label3";
-            label3.Size = new Size(17, 15);
-            label3.TabIndex = 1;
-            label3.Text = "Y:";
-            // 
-            // inputCy
-            // 
-            inputCy.Location = new Point(51, 78);
-            inputCy.Name = "inputCy";
-            inputCy.Size = new Size(83, 23);
-            inputCy.TabIndex = 2;
+            label9.AutoSize = true;
+            label9.Location = new Point(660, 124);
+            label9.Name = "label9";
+            label9.Size = new Size(17, 15);
+            label9.TabIndex = 1;
+            label9.Text = "Z:";
             // 
             // label4
             // 
@@ -139,12 +144,46 @@
             label4.TabIndex = 1;
             label4.Text = "Z:";
             // 
-            // inputCz
+            // inputCubeSize
             // 
-            inputCz.Location = new Point(51, 116);
-            inputCz.Name = "inputCz";
-            inputCz.Size = new Size(83, 23);
-            inputCz.TabIndex = 2;
+            inputCubeSize.Location = new Point(245, 78);
+            inputCubeSize.Name = "inputCubeSize";
+            inputCubeSize.Size = new Size(83, 23);
+            inputCubeSize.TabIndex = 2;
+            inputCubeSize.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            inputCubeSize.ValueChanged += CubeSide_ValueChanged;
+            // 
+            // inputCy
+            // 
+            inputCy.Location = new Point(51, 78);
+            inputCy.Name = "inputCy";
+            inputCy.Size = new Size(83, 23);
+            inputCy.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(28, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(17, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Y:";
+            // 
+            // inputCx
+            // 
+            inputCx.Location = new Point(51, 37);
+            inputCx.Name = "inputCx";
+            inputCx.Size = new Size(83, 23);
+            inputCx.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 15);
+            label2.TabIndex = 1;
+            label2.Text = "X:";
             // 
             // label5
             // 
@@ -156,12 +195,25 @@
             label5.TabIndex = 0;
             label5.Text = "Cube side size:";
             // 
-            // inputCubeSize
+            // label6
             // 
-            inputCubeSize.Location = new Point(245, 78);
-            inputCubeSize.Name = "inputCubeSize";
-            inputCubeSize.Size = new Size(83, 23);
-            inputCubeSize.TabIndex = 2;
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(683, 104);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 15);
+            label6.TabIndex = 0;
+            label6.Text = "K (0, 0, z):";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Cube Center coordinates:";
             // 
             // Form1
             // 
@@ -171,22 +223,23 @@
             Controls.Add(splitContainer1);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)_mainView).EndInit();
             splitContainer1.Panel1.ResumeLayout(false);
             splitContainer1.Panel2.ResumeLayout(false);
             splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)inputCx).EndInit();
-            ((System.ComponentModel.ISupportInitialize)inputCy).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputKz).EndInit();
             ((System.ComponentModel.ISupportInitialize)inputCz).EndInit();
             ((System.ComponentModel.ISupportInitialize)inputCubeSize).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputCy).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputCx).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
+        private PictureBox _mainView;
         private SplitContainer splitContainer1;
         private Label label1;
         private NumericUpDown inputCz;
@@ -197,5 +250,9 @@
         private Label label2;
         private NumericUpDown inputCubeSize;
         private Label label5;
+        private NumericUpDown inputKz;
+        private Label label9;
+        private Label label6;
+        private Button buttonDraw;
     }
 }
--- Lab-s/4/Solution/WF/Form1.cs
+++ Lab-s/4/Solution/WF/Form1.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace WF
 {
     public partial class Form1 : Form
@@ -5,6 +7,74 @@ namespace WF
         public Form1()
         {
             InitializeComponent();
+
+            _mainView.Image = new Bitmap(_mainView.Width, _mainView.Height);
+
+            _g = Graphics.FromImage(_mainView.Image);
+
+            inputKz.Maximum = uint.MaxValue;
+            inputCubeSize.Maximum = ushort.MaxValue;
+        }
+
+        private uint K = 100;
+        private float r => 1f / K;
+
+        private Cube _cube = new Cube(new Vector3(0, 0, 20), 10);
+
+        private Graphics _g;
+        private Pen _pen = new Pen(Color.Black);
+
+        private void DrawCube()
+        {
+            MainViewClear();
+
+            foreach (var item in _cube.Edges)
+            {
+                var buffer = GraphicsExtensions.Vector3ToVector4(item.Item1);
+
+                var p1 = GraphicsExtensions.Vector4ToVector3(Vector4.Transform(buffer, GraphicsExtensions.PerspectiveMatrix(r)));
+
+                buffer = GraphicsExtensions.Vector3ToVector4(item.Item2);
+
+                var p2 = GraphicsExtensions.Vector4ToVector3(Vector4.Transform(buffer, GraphicsExtensions.PerspectiveMatrix(r)));
+
+
+                _g.DrawLine(_pen,
+                    ScreenCenterX + p1.X, ScreenCenterY - p1.Y,
+                    ScreenCenterX + p2.X, ScreenCenterY - p2.Y);
+            }
+
+            _mainView.Image = (Bitmap)_mainView.Image;
+        }
+
+        private float ScreenCenterX => _mainView.Image.Width / 2f;
+        private float ScreenCenterY => _mainView.Image.Height / 2f;
+
+        private void MainView_SizeChanged(object sender, EventArgs e)
+        {
+            _mainView.Image = new Bitmap(_mainView.Width, _mainView.Height);
+            _g = Graphics.FromImage(_mainView.Image);
+        }
+
+        private void ButtonDraw_Click(object sender, EventArgs e)
+        {
+            DrawCube();
+        }
+
+        private void Kz_ValueChanged(object sender, EventArgs e)
+        {
+            K = (uint)inputKz.Value;
+        }
+
+        private void MainViewClear()
+        {
+            _mainView.Image = new Bitmap(_mainView.Image.Width, _mainView.Image.Height);
+            _g = Graphics.FromImage(_mainView.Image);
+        }
+
+        private void CubeSide_ValueChanged(object sender, EventArgs e)
+        {
+            _cube.SideLength = (ushort)inputCubeSize.Value;
         }
     }
 }
