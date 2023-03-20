ac6a26aed6b313239519d37d6a40da84aad1179b
NemoNology <chicken.gang@mail.ru>
2023-03-21 08:02:12 +0900

    Added constructors & Edges prop

--- Lab-s/4/Solution/WF/Cube.cs
+++ Lab-s/4/Solution/WF/Cube.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace WF
 {
@@ -29,6 +30,18 @@ namespace WF
         /// Rotation that change points on X & Y - axises
         /// </summary>
         public ushort RotationByAxisZ { get; set; }
+
+        public Cube()
+        {
+            Center = Vector3.Zero;
+            SideLength = 10;
+        }
+
+        public Cube(Vector3 center, ushort sideLength)
+        {
+            Center = center;
+            SideLength = sideLength;
+        }
 
         private float HalfSideLenght => SideLength / 2f;
 
@@ -117,6 +130,33 @@ namespace WF
             get
             {
                 return new Vector3(Center.X + HalfSideLenght, Center.Y - HalfSideLenght, Center.Z - HalfSideLenght);
+            }
+        }
+
+        /// <summary>
+        /// List of cube edges, where item (Vector3) - 3D line; <br/>
+        /// Item1.x, y, z - first point of line, Item2.x, y, z - second; 
+        /// </summary>
+        public List<Tuple<Vector3, Vector3>> Edges
+        {
+            get
+            {
+                return new List<Tuple<Vector3, Vector3>> {
+                    new Tuple<Vector3, Vector3>(A1, A2),
+                    new Tuple<Vector3, Vector3>(A2, A3),
+                    new Tuple<Vector3, Vector3>(A3, A4),
+                    new Tuple<Vector3, Vector3>(A4, A1),
+
+                    new Tuple<Vector3, Vector3>(B1, B2),
+                    new Tuple<Vector3, Vector3>(B2, B3),
+                    new Tuple<Vector3, Vector3>(B3, B4),
+                    new Tuple<Vector3, Vector3>(B4, B1),
+
+                    new Tuple<Vector3, Vector3>(A1, B1),
+                    new Tuple<Vector3, Vector3>(A2, B2),
+                    new Tuple<Vector3, Vector3>(A3, B3),
+                    new Tuple<Vector3, Vector3>(A4, B4),
+                };
             }
         }
     }
