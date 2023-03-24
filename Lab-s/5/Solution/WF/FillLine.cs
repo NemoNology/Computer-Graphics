a8c59c1345e6db914d31109f64d6b6394d8bcfda
NemoNology <chicken.gang@mail.ru>
2023-03-24 21:58:57 +0900

    Optimized & Not Tested
    To do: Tests

--- Lab-s/5/Solution/WF/FillLine.cs
+++ Lab-s/5/Solution/WF/FillLine.cs
@@ -2,103 +2,67 @@
 {
     internal class FillLine : IFillingAlghoritm
     {
-        private List<Point> Branches { get; set; } = new List<Point>();
+        private Stack<Point> Branches { get; set; } = new Stack<Point>();
 
         public void Fill(ref Bitmap bmp, Point startFillPoint, Color fillColor)
         {
             var voidColor = bmp.GetPixel(startFillPoint.X, startFillPoint.Y);
 
-            Branches.Add(startFillPoint);
+            Branches.Push(startFillPoint);
 
             while (Branches.Count > 0)
             {
-                FillHorizon(bmp, Branches.First(),
-                fillColor, voidColor);
+                FillHorizon(bmp, Branches.Pop(),
+                fillColor, voidColor, false);
             }
-        }
 
-        private bool IsValidPoint(Bitmap bmp, Point point, Color voidColor)
-        {
-            var x = point.X;
-            var y = point.Y;
+            Branches.Push(startFillPoint);
 
-            if ((x >= 0 && x < bmp.Width) &&
-                (y >= 0 && y < bmp.Height) &&
-                bmp.GetPixel(x, y) == voidColor)
+            while (Branches.Count > 0)
             {
-                return true;
+                FillHorizon(bmp, Branches.Pop(),
+                fillColor, voidColor, true);
             }
-
-            return false;
         }
 
         private void FillHorizon(Bitmap bmp, Point startFillPoint,
-        Color fillColor, Color voidColor)
+        Color fillColor, Color voidColor, bool IsDown)
         {
             // Left & Right Points
             var lp = new MyPoint(startFillPoint.X, startFillPoint.Y);
-            var rp = lp.Copy;
 
-            while (IsValidPoint(bmp, lp.PointWithMove(-1), voidColor))
+            // Move Left Point to Left
+            while (lp.X - 1 >= 0 && bmp.GetPixel(lp.X - 1, lp.Y) == voidColor)
             {
                 lp.X--;
             }
 
-            rp = lp.Copy;
+            var rp = lp.Copy;
 
-            bool upC = false;
-            bool downC = false;
+            bool IsOnTheSameHeight = false;
 
-            while (IsValidPoint(bmp, rp.PointWithMove(1), voidColor))
+            int Dy = IsDown ? 1 : -1;
+
+            // Going to Right and save Up/Down point, if it's possible
+            while (rp.X < bmp.Width && bmp.GetPixel(rp.X, rp.Y) == voidColor)
             {
+                bmp.SetPixel(rp.X, rp.Y, fillColor);
+
                 rp.X++;
 
-				var canUp = IsValidPoint(bmp, rp.PointWithMove(0, -1), voidColor);
+                var checkVertical = rp.Y + Dy >= 0 && bmp.GetPixel(lp.X, lp.Y + Dy) == voidColor;
 
-                if (!upC && canUp)
+                if (!IsOnTheSameHeight && checkVertical)
                 {
-                    Branches.Add(rp.PointWithMove(0, -1));
-                    upC = true;
+                    Branches.Push(rp.PointWithMove(0, Dy));
+                    IsOnTheSameHeight = true;
                 }
-                else if (canUp)
+                else if (!checkVertical)
                 {
-                    upC = true;
-                }
-                else
-                {
-                    upC = false;
-                }
-
-				var canDown = IsValidPoint(bmp, rp.PointWithMove(0, 1), voidColor);
-
-                if (!downC && canDown)
-                {
-                    Branches.Add(rp.PointWithMove(0, 1));
-                    downC = true;
-                }
-                else if (canDown)
-                {
-                    downC = true;
-                }
-                else
-                {
-                    downC = false;
+                    IsOnTheSameHeight = false;
+                    checkVertical = true;
                 }
             }
-
-            bmp = DrawLine(bmp, lp.X, rp.X, lp.Y, fillColor);
-
-            Branches = Branches.FindAll(x => IsValidPoint(bmp, x, voidColor));
-        }
-
-        private Bitmap DrawLine(Bitmap bmp, int x1, int x2, int y, Color lineColor)
-        {
-            for (var i = x1; i <= x2; i++)
-            {
-                bmp.SetPixel(i, y, lineColor);
-            }
-            
-            return bmp;
         }
     }
 
@@ -106,8 +70,6 @@
     {
         public int X { get; set; } = 0;
         public int Y { get; set; } = 0;
-
-        public Point Point => new Point(X, Y);
 
         public MyPoint(int x, int y)
         {
@@ -127,16 +89,5 @@
         }
 
         public MyPoint Copy => new MyPoint(this);
-
-
-        public static bool operator ==(MyPoint p1, MyPoint p2)
-        {
-            return p1.X == p2.X && p1.Y == p2.Y;
-        }
-
-        public static bool operator !=(MyPoint p1, MyPoint p2)
-        {
-            return p1.X != p2.X || p1.Y != p2.Y;
-        }
     }
 }
