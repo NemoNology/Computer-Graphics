e9a229f1980a32faebe8eaa971e6ed4ffe491108
NemoNology <chicken.gang@mail.ru>
2023-03-29 06:17:00 +0900

    Build 1.0

--- Lab-s/6/Project/MainForm.cs
+++ Lab-s/6/Project/MainForm.cs
@@ -105,18 +105,22 @@ public partial class MainForm : Form
     {
         if (_lines.Count == 0) return;
 
-        Point p1, p2;
+        Vector2 p1, p2;
 
         if (DrawOnlyLastLine)
         {
             Vector4 l = _lines.Last();
 
-            p1 = new Point((int)l.X, (int)l.Y);
-            p2 = new Point((int)l.Z, (int)l.W);
+            p1 = new Vector2(l.X, l.Y);
+            p2 = new Vector2(l.Z, l.W);
 
-            _g.DrawLine(
-                new Pen(Color.Blue), 
-                p1, p2);
+            if (CohenSutherlandLineClip(ref p1, ref p2))
+            {
+                _g.DrawLine(
+                    new Pen(Color.Blue),
+                    p1.X, p1.Y,
+                    p2.X, p2.Y);
+            }
 
             Redraw(true);
 
@@ -125,12 +129,16 @@ public partial class MainForm : Form
 
         foreach (var line in _lines)
         {
-            p1 = new Point((int)line.X, (int)line.Y);
-            p2 = new Point((int)line.Z, (int)line.W);
+            p1 = new Vector2(line.X, line.Y);
+            p2 = new Vector2(line.Z, line.W);
 
-            _g.DrawLine(
-                new Pen(Color.Blue), 
-                p1, p2);
+            if (CohenSutherlandLineClip(ref p1, ref p2))
+            {
+                _g.DrawLine(
+                    new Pen(Color.Blue),
+                    p1.X, p1.Y,
+                    p2.X, p2.Y);
+            }
 
             Redraw(true);
         }
@@ -188,5 +196,125 @@ public partial class MainForm : Form
         outputMainView.Image = (Bitmap)outputMainView.Image;
     }
 
-    
+    private byte CodePoint(Vector2 point)
+    {
+        byte res = 0;
+
+        // LEFT - 0001
+        if (point.X < _square.A.X)
+        {
+            res += 1;
+        }
+        // RIGHT - 0010
+        else if (point.X > _square.C.X)
+        {
+            res += 2;
+        }
+
+        // BOTTOM - 0100
+        if (point.Y > _square.C.Y)
+        {
+            res += 4;
+        }
+        // TOP - 1000
+        else if (point.Y < _square.A.Y)
+        {
+            res += 8;
+        }
+
+        return res;
+    }
+
+    private bool CohenSutherlandLineClip(ref Vector2 point1, ref Vector2 point2)
+    {
+        // Calc points places
+        var code1 = CodePoint(point1);
+        var code2 = CodePoint(point2);
+
+        // Is line or line part visible
+        bool accept = false;
+
+        float x = 0, y = 0;
+        float x0 = point1.X, y0 = point1.Y;
+        float x1 = point2.X, y1 = point2.Y;
+
+        // Our window is square
+
+        while (true)
+        {
+            // If line already in square
+            if (code1 + code2 == 0)
+            {
+                accept = true;
+                break;
+            }
+            // Line don't cross square
+            else if ((code1 & code2) != 0)
+            {
+                break;
+            }
+            // Line cross square
+            else
+            {
+                float minX = _square.A.X, maxX = _square.C.X;
+                float minY = _square.A.Y, maxY = _square.C.Y;
+
+                // Choosing point, that is not inside square
+                var codeOut = code2 > code1 ? code2 : code1;
+
+                // Now find the intersection point;
+                // Use formulas:
+                //   slope = (y1 - y0) / (x1 - x0)
+                //   x = x0 + (1 / slope) * (ym - y0), where ym is Y min or Y max
+                //   y = y0 + slope * (xm - x0), where xm is X min or X max
+                // No need to worry about divide-by-zero because, in each case, the
+                // code bit being tested guarantees the denominator is non-zero
+
+                // Point is above square
+                if ((codeOut & 8) != 0)
+                {
+                    x = x0 + (x1 - x0) * (minY - y0) / (y1 - y0);
+                    y = minY;
+                }
+                // Point is below square
+                else if ((codeOut & 4) > 0)
+                {
+                    x = x0 + (x1 - x0) * (maxY - y0) / (y1 - y0);
+                    y = maxY;
+                }
+                // Point is to the right of square
+                else if ((codeOut & 2) > 0)
+                {
+                    y = y0 + (y1 - y0) * (maxX - x0) / (x1 - x0);
+                    x = maxX;
+                }
+                // Point is to the left of square
+                else if ((codeOut & 1) > 0)
+                {
+                    y = y0 + (y1 - y0) * (minX - x0) / (x1 - x0);
+                    x = minX;
+                }
+
+                // Now we move outside point to intersection point to clip
+                // and get ready for next pass.
+                if (codeOut == code1)
+                {
+                    x0 = x;
+                    y0 = y;
+                    code1 = CodePoint(new Vector2(x0, y0));
+                }
+                else
+                {
+                    x1 = x;
+                    y1 = y;
+                    code2 = CodePoint(new Vector2(x1, y1));
+                }
+            }
+        }
+
+        point1 = new Vector2(x0, y0);
+        point2 = new Vector2(x1, y1);
+
+        return accept;
+    }
 }
