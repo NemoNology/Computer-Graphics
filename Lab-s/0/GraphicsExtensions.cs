c35619573fbf3e06c3c6598f7b7bf11550e89ed1
NemoNology <chicken.gang@mail.ru>
2023-04-11 07:47:57 +0900

    Build 1.0 - Minimum basis

--- Lab-s/0/GraphicsExtensions.cs
+++ Lab-s/0/GraphicsExtensions.cs
@@ -7,16 +7,15 @@ namespace WF
         /// <summary>
         /// Transform 3d point to 4d point for future manipulations
         /// </summary>
-        public static Vector4 Vector3ToVector4(Vector3 v)
+        public Vector4 Vector3ToVector4(Vector3 v)
         {
             return new Vector4(v, 1);
         }
 
-        /// <summary>
-        /// Z - Axis 0, Y - Axis 1, X - Axis 2;
-        /// </summary>
-        /// <param name="r"> coefficient: 1 / k </param>
-        public static Matrix4x4 PerspectiveMatrix(float r, int axis = 0)
+        /// <param name="r"> Coefficient: 1 / k - Focus distance? </param>
+        /// <param name="axis"> 0 - Z Axis <br/> 1 - Y Axis <br/> 2 - X Axis </param>
+        /// <returns> Matrix for transformation by chosen axis </returns>
+        public Matrix4x4 PerspectiveMatrix(float r, int axis = 0)
         {
             switch (axis)
             {
@@ -56,13 +55,30 @@ namespace WF
         /// <summary>
         /// Transform 4d point to usual 3d point after manipulations
         /// </summary>
-        public static Vector3 Vector4ToVector3(Vector4 v)
+        public Vector3 Vector4ToVector3(Vector4 v)
         {
             return new Vector3(
                 v.X / v.W, 
                 v.Y / v.W, 
                 v.Z / v.W);
         }
+
+        /// <summary>
+        /// Transform Vector3 to center projection
+        /// </summary>
+        /// <param name="v"> Vector that will be transformed </param>
+        /// <param name="r"> Focus distance? </param>
+        /// <param name="axis"> Axis by that will be transformed vector </param>
+        /// <returns> Transformed vector </returns>
+        public Vector3 TransformVector(Vector3 v, float r, int axis = 0)
+        {
+            return Vector4ToVector3(Vector4.Transform
+                (Vector3ToVector4(v), 
+                PerspectiveMatrix(r, axis)
+                ));
+        }
+
+
 
     }
 }
