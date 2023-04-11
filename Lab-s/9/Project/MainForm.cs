dfdad7603c6a7e352d3a2a42e400b4329643ba01
NemoNology <chicken.gang@mail.ru>
2023-04-11 13:36:04 +0900

    Build 0.22

--- Lab-s/9/Project/MainForm.cs
+++ Lab-s/9/Project/MainForm.cs
@@ -7,16 +7,61 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+
+        mainView.Image = new Bitmap(mainView.Width, mainView.Height);
+
+        // _particleSystem = new ParticleSystem(
+        //     FireSprite, CenterPoint,
+        //     new Vector3(15, 15, 15), 
+        //     new Vector3(-3, -3, -3));
+        // _floor = new Floor3D(CenterPoint, 200, 10);
+
+        var test01 = Vector3.Zero;
+        var test02 = new Vector3(0, 0, 50);
+
+        _g = Graphics.FromImage(mainView.Image);
+
+        test01 = _ge.TransformVector(test01, R);
+        test02 = _ge.TransformVector(test02, R);
+
+        _g.DrawLine(
+            new Pen(Color.Black), 
+            test01.X + CenterPoint.X, test01.Y + CenterPoint.Y, 
+            test02.X + CenterPoint.X, test02.Y + CenterPoint.Y);
+        
     }
 
     private const float R = 0.01f;
     private ParticleSystem _particleSystem;
+    private Floor3D _floor;
+    private Graphics _g;
+
+    private Image FireSprite = Bitmap.FromFile($"{Application.StartupPath}\\Data\\Sprites\\FireSprite.png"); 
 
     private GraphicUnit.GraphicsExtensions _ge = new GraphicUnit.GraphicsExtensions();
 
     private void MainForm_Resize(object sender, EventArgs e)
     {
+        mainView.Image = new Bitmap(mainView.Width, mainView.Height);
 
+        _particleSystem = new ParticleSystem(
+            FireSprite, CenterPoint,
+            new Vector3(15, 15, 15), 
+            new Vector3(-3, -3, -3));
+        _floor = new Floor3D(CenterPoint, 200, 10);
+
+        var test01 = CenterPoint;
+        var test02 = new Vector3(CenterPoint.X, 0, 50);
+
+        _g = Graphics.FromImage(mainView.Image);
+
+        test01 = _ge.TransformVector(test01, R);
+        test02 = _ge.TransformVector(test02, R);
+
+        _g.DrawLine(
+            new Pen(Color.Black), 
+            test01.X, test01.Y, 
+            test02.X, test02.Y);
     }
 
     private Vector3 CenterPoint
@@ -25,7 +70,7 @@ public partial class MainForm : Form
         {
             return new Vector3(
                 mainView.Image.Width / 2f,
-                mainView.Image.Height / 2f,
+                mainView.Image.Height * 0.8f,
                 0);
         }
     }
