52fed118a3842ac04e19a6ae13825858a22b24ec
NemoNology <chicken.gang@mail.ru>
2023-04-15 13:08:23 +0900

    Added extra Translation & Added Rotation

--- Lab-s/9/Project/Floor.cs
+++ Lab-s/9/Project/Floor.cs
@@ -114,7 +114,7 @@ namespace Project
         /// <param name="Dx"> The value, along the X axis, by which the floor will move </param>
         /// <param name="Dy"> The value, along the Y axis, by which the floor will move </param>
         /// <param name="Dz"> The value, along the Z axis, by which the floor will move </param>
-        public async void Translate(float Dx, float Dy = 0, float Dz = 0)
+        public void Translate(float Dx, float Dy = 0, float Dz = 0)
         {
             var translateMatrix = new Matrix4x4(
                 1, 0, 0, 0,
@@ -123,19 +123,16 @@ namespace Project
                 Dx, Dy, Dz, 1
             );
 
-            await Task.Run(() =>
+            for (int i = 0; i < Points.Count; i++)
             {
-                for (int i = 0; i < Points.Count; i++)
-                {
-                    var buffer = new Vector4(Points[i], 1);
-                    buffer = Vector4.Transform(buffer, translateMatrix);
-                    Points[i] = new Vector3(
-                        buffer.X,
-                        buffer.Y,
-                        buffer.Z
-                        );
-                }
-            });
+                var buffer = new Vector4(Points[i], 1);
+                buffer = Vector4.Transform(buffer, translateMatrix);
+                Points[i] = new Vector3(
+                    buffer.X,
+                    buffer.Y,
+                    buffer.Z
+                    );
+            }
 
             Parallel.ForEach(_cells, cell =>
             {
@@ -143,6 +140,86 @@ namespace Project
             });
         }
 
-        // TODO: Rotation
+        /// <summary>
+        /// Rotation of a floor around a rotation center point by certain angles
+        /// </summary>
+        /// <param name="rotationCenterPoint"> The point around which the floor revolves </param>
+        /// <param name="angleDegreeByX"> Angle in degrees along the X axis by which the floor will rotate </param>
+        /// <param name="angleDegreeByY"> Angle in degrees along the Y axis by which the floor will rotate </param>
+        /// <param name="angleDegreeByZ"> Angle in degrees along the Z axis by which the floor will rotate </param>
+        public void RotateAt(Vector3 rotationCenterPoint, float angleDegreeByX = 0,
+            float angleDegreeByY = 0, float angleDegreeByZ = 0)
+        {
+            double rad = -Math.PI / 180;
+
+            float cosX = (float)Math.Cos(rad * angleDegreeByX);
+            float sinX = (float)Math.Sin(rad * angleDegreeByX);
+
+            float cosY = (float)Math.Cos(rad * angleDegreeByY);
+            float sinY = (float)Math.Sin(rad * angleDegreeByY);
+
+            float cosZ = (float)Math.Cos(rad * angleDegreeByZ);
+            float sinZ = (float)Math.Sin(rad * angleDegreeByZ);
+
+            var rotationMatrixX = new Matrix4x4(
+                1, 0_00f, 0_00f, 0,
+                0, cosX,  sinX,  0,
+                0, -sinX, cosX,  0,
+                0, 0_00f, 0_00f, 1
+            );
+
+            var rotationMatrixY = new Matrix4x4(
+                cosY,  0, sinY,  0,
+                0_00f, 1, 0_00f, 0,
+                -sinY, 0, cosY,  0,
+                0_00f, 0, 0_00f, 1
+            );
+
+            var rotationMatrixZ = new Matrix4x4(
+                 cosZ, sinZ,  0, 0,
+                -sinZ, cosZ,  0, 0,
+                0_00f, 0_00f, 1, 0,
+                0_00f, 0_00f, 0, 1
+            );
+
+            Matrix4x4 rotationMatrix =
+                rotationMatrixX * rotationMatrixY * rotationMatrixZ;
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var buffer = new Vector4(Points[i], 1);
+                buffer = Vector4.Transform(buffer, rotationMatrix);
+                Points[i] = new Vector3(
+                    buffer.X,
+                    buffer.Y,
+                    buffer.Z
+                    );
+            }
+
+            Parallel.ForEach(_cells, cell =>
+            {
+                cell.Rotate(rotationMatrix);
+            });
+
+            Translate(
+                rotationCenterPoint.X, 
+                rotationCenterPoint.Y, 
+                rotationCenterPoint.Z);
+        }
+    
+        public Vector3 CenterPoint
+        {
+            get
+            {
+                var a = A;
+                var c = C;
+
+                return new Vector3(
+                    (a.X + c.X) / 2,
+                    (a.Y + c.Y) / 2,
+                    (a.Z + c.Z) / 2
+                    );
+            }
+        }
     }
 }
--- Lab-s/9/Project/Square3D.cs
+++ Lab-s/9/Project/Square3D.cs
@@ -53,30 +53,126 @@ namespace Project
         /// <param name="Dx"> The value, along the X axis, by which the square will move </param>
         /// <param name="Dy"> The value, along the Y axis, by which the square will move </param>
         /// <param name="Dz"> The value, along the Z axis, by which the square will move </param>
-        public async void Translate(float Dx, float Dy = 0, float Dz = 0)
+        public void Translate(float Dx, float Dy = 0, float Dz = 0)
         {
             var translateMatrix = new Matrix4x4(
+
                 1,  0,  0,  0,
                 0,  1,  0,  0,
                 0,  0,  1,  0,
                 Dx, Dy, Dz, 1
             );
 
-            await Task.Run(() =>
+            for (int i = 0; i < Points.Count; i++)
             {
-                for (int i = 0; i < Points.Count; i++)
-                {
-                    var buffer = new Vector4(Points[i], 1);
-                    buffer = Vector4.Transform(buffer, translateMatrix);
-                    Points[i] = new Vector3(
-                        buffer.X,
-                        buffer.Y,
-                        buffer.Z
-                        );
-                }
-            });
+                var buffer = new Vector4(Points[i], 1);
+                buffer = Vector4.Transform(buffer, translateMatrix);
+                Points[i] = new Vector3(
+                    buffer.X,
+                    buffer.Y,
+                    buffer.Z
+                    );
+            }
         }
-    
-        // TODO: Rotation
+
+        /// <summary>
+        /// Move square by translation matrix
+        /// </summary>
+        public void Translate(Matrix4x4 translateMatrix)
+        {
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var buffer = new Vector4(Points[i], 1);
+                buffer = Vector4.Transform(buffer, translateMatrix);
+                Points[i] = new Vector3(
+                    buffer.X,
+                    buffer.Y,
+                    buffer.Z
+                    );
+            }
+        }
+
+        /// <summary>
+        /// Rotation of a square around a rotation center point by certain angles
+        /// </summary>
+        /// <param name="rotationCenterPoint"> The point around which the square revolves </param>
+        /// <param name="angleDegreeByX"> Angle in degrees along the X axis by which the square will rotate </param>
+        /// <param name="angleDegreeByY"> Angle in degrees along the Y axis by which the square will rotate </param>
+        /// <param name="angleDegreeByZ"> Angle in degrees along the Z axis by which the square will rotate </param>
+        public void RotateAt(Vector3 rotationCenterPoint, float angleDegreeByX = 0,
+            float angleDegreeByY = 0, float angleDegreeByZ = 0)
+        {
+            double rad = -Math.PI / 180;
+
+            float cosX = (float)Math.Cos(rad * angleDegreeByX);
+            float sinX = (float)Math.Sin(rad * angleDegreeByX);
+
+            float cosY = (float)Math.Cos(rad * angleDegreeByY);
+            float sinY = (float)Math.Sin(rad * angleDegreeByY);
+
+            float cosZ = (float)Math.Cos(rad * angleDegreeByZ);
+            float sinZ = (float)Math.Sin(rad * angleDegreeByZ);
+
+            var rotationMatrixX = new Matrix4x4(
+                1, 0_00f, 0_00f, 0,
+                0, cosX,  sinX,  0,
+                0, -sinX, cosX,  0,
+                0, 0_00f, 0_00f, 1
+            );
+
+            var rotationMatrixY = new Matrix4x4(
+                cosY,  0, sinY,  0,
+                0_00f, 1, 0_00f, 0,
+                -sinY, 0, cosY,  0,
+                0_00f, 0, 0_00f, 1
+            );
+
+            var rotationMatrixZ = new Matrix4x4(
+                 cosZ, sinZ,  0, 0,
+                -sinZ, cosZ,  0, 0,
+                0_00f, 0_00f, 1, 0,
+                0_00f, 0_00f, 0, 1
+            );
+
+            Matrix4x4 rotationMatrix = 
+                rotationMatrixX * rotationMatrixY * rotationMatrixZ;
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var buffer = new Vector4(Points[i], 1);
+                buffer = Vector4.Transform(buffer, rotationMatrix);
+                Points[i] = new Vector3(
+                    buffer.X,
+                    buffer.Y,
+                    buffer.Z
+                    );
+            }
+
+            var translateMatrix = new Matrix4x4(
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                rotationCenterPoint.X, rotationCenterPoint.Y, rotationCenterPoint.Z, 1
+            );
+
+            Translate(translateMatrix);
+        }
+
+        /// <summary>
+        /// Rotation of a square by rotation matrix
+        /// </summary>
+        public void Rotate(Matrix4x4 rotationMatrix)
+        {
+            for (int i = 0; i < Points.Count; i++)
+            {
+                var buffer = new Vector4(Points[i], 1);
+                buffer = Vector4.Transform(buffer, rotationMatrix);
+                Points[i] = new Vector3(
+                    buffer.X,
+                    buffer.Y,
+                    buffer.Z
+                    );
+            }
+        }
     }
 }
