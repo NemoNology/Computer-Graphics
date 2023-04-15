32f584cadbdf6a95c9fe2e187fbb7373ff46f6e6
NemoNology <chicken.gang@mail.ru>
2023-04-15 16:03:35 +0900

    Fixed method name & one mistake

--- Lab-s/9/Project/IObject3D.cs
+++ Lab-s/9/Project/IObject3D.cs
@@ -18,12 +18,12 @@ namespace Project
         /// <param name="rotationPoint"> The point around which the Object3D revolves </param>
         /// <param name="rotationAngleDegree"> Rotation angle in degrees </param>
         /// <param name="rotationAxis"> Axis X - 0 <br/> Axis Y - 1 <br/> Axis Z - 2 </param>
-        public void RotateAt(float rotationAngleDegree, int rotationAxis);
+        public void Rotate(float rotationAngleDegree, int rotationAxis);
 
         /// <summary>
         /// Transform Object3D by transform matrix
         /// </summary>
-        /// <param name="transformMatrix"> Matrix by which Object3D will be transformed </param>
+        /// <param name="transformMatrix"> Matrix with which Object3D will be transformed </param>
         public void Transform(Matrix4x4 transformMatrix);
 
     }
