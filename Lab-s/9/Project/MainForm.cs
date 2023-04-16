e8fb19c031cbaa5041601dc9cfc45e8f20542eea
NemoNology <chicken.gang@mail.ru>
2023-04-17 00:23:51 +0900

    Added hotkeys & floor: draw, rotate, move

--- Lab-s/9/Project/MainForm.cs
+++ Lab-s/9/Project/MainForm.cs
@@ -10,70 +10,245 @@ public partial class MainForm : Form
 
         mainView.Image = new Bitmap(mainView.Width, mainView.Height);
 
+        _screenCenter = new Point3D(
+            mainView.Image.Width / 2f,
+            mainView.Image.Height * 0.8f,
+            0);
+
         // _particleSystem = new ParticleSystem(
-        //     FireSprite, CenterPoint,
+        //     FireSprite, new Vector3(0, 0, 100),
         //     new Vector3(15, 15, 15), 
         //     new Vector3(-3, -3, -3));
-        // _floor = new Floor3D(CenterPoint, 200, 10);
 
-        var test01 = Vector3.Zero;
-        var test02 = new Vector3(0, 0, 50);
+        _floor = new Floor3D(
+            new Point3D(
+                0,
+                -_screenCenter.Y * 0.2f,
+                mainView.Width / 2f),
+            mainView.Width
+            );
 
-        _g = Graphics.FromImage(mainView.Image);
-
-        test01 = _ge.TransformVector(test01, R);
-        test02 = _ge.TransformVector(test02, R);
-
-        _g.DrawLine(
-            new Pen(Color.Black), 
-            test01.X + CenterPoint.X, test01.Y + CenterPoint.Y, 
-            test02.X + CenterPoint.X, test02.Y + CenterPoint.Y);
-        
+        DrawFloor();
     }
 
-    private const float R = 0.01f;
+    private const float R = 1 / 200f;
     private ParticleSystem _particleSystem;
     private Floor3D _floor;
     private Graphics _g;
 
-    private Image FireSprite = Bitmap.FromFile($"{Application.StartupPath}\\Data\\Sprites\\FireSprite.png"); 
+    private Square3D _square;
 
-    private GraphicUnit.GraphicsExtensions _ge = new GraphicUnit.GraphicsExtensions();
+    private Point3D _screenCenter;
+
+    private Image FireSprite = Bitmap.FromFile($"{Application.StartupPath}\\Data\\Sprites\\FireSprite.png");
 
     private void MainForm_Resize(object sender, EventArgs e)
     {
         mainView.Image = new Bitmap(mainView.Width, mainView.Height);
 
-        _particleSystem = new ParticleSystem(
-            FireSprite, CenterPoint,
-            new Vector3(15, 15, 15), 
-            new Vector3(-3, -3, -3));
-        _floor = new Floor3D(CenterPoint, 200, 10);
+        _screenCenter = new Point3D(
+            mainView.Image.Width / 2f,
+            mainView.Image.Height * 0.8f,
+            0);
 
-        var test01 = CenterPoint;
-        var test02 = new Vector3(CenterPoint.X, 0, 50);
+        // TODO: Particle System initialization & Floor Drawing
 
-        _g = Graphics.FromImage(mainView.Image);
+        //_particleSystem = new ParticleSystem(
+        //    FireSprite, new Vector3(0, 0, 100),
+        //    new Vector3(15, 15, 15), 
+        //    new Vector3(-3, -3, -3));
 
-        test01 = _ge.TransformVector(test01, R);
-        test02 = _ge.TransformVector(test02, R);
+        _floor = new Floor3D(
+            new Point3D(
+                0,
+                -_screenCenter.Y * 0.2f,
+                mainView.Width / 2f),
+            mainView.Width);
 
-        _g.DrawLine(
-            new Pen(Color.Black), 
-            test01.X, test01.Y, 
-            test02.X, test02.Y);
+        DrawFloor();
     }
 
-    private Vector3 CenterPoint
+    private void DrawSquare()
     {
-        get
+        MainViewClear();
+
+        var square = new Square3D(new Point3D(-50, -50, 100), 100);
+
+        foreach (var edge in square.GetEdgesInCentralProjection(R))
         {
-            return new Vector3(
-                mainView.Image.Width / 2f,
-                mainView.Image.Height * 0.8f,
-                0);
+            _g.DrawLine(new Pen(Color.Gray),
+                _screenCenter.X + edge.point1.X, _screenCenter.Y - edge.point1.Y,
+                _screenCenter.X + edge.point2.X, _screenCenter.Y - edge.point2.Y);
         }
+
+        square.RotateAt(square.CenterPoint, 45);
+
+        _g.DrawEllipse(new Pen(Color.Magenta),
+            square.CenterPoint.GetCentralProjection(R).X,
+            square.CenterPoint.GetCentralProjection(R).Y,
+            2, 2);
+
+        foreach (var edge in square.GetEdgesInCentralProjection(R))
+        {
+            _g.DrawLine(new Pen(Color.Black),
+                _screenCenter.X + edge.point1.X, _screenCenter.Y - edge.point1.Y,
+                _screenCenter.X + edge.point2.X, _screenCenter.Y - edge.point2.Y);
+        }
+
+        mainView.Image = (Bitmap)mainView.Image;
     }
 
+    private void DrawFloor()
+    {
+        if (_floor == null)
+        {
+            return;
+        }
 
+        MainViewClear();
+
+        foreach (var edge in _floor.Grid)
+        {
+            // TODO: if floor cells amount = 4: method TransformVector return infinity
+
+            var p1 = edge.point1.GetCentralProjection(R);
+            var p2 = edge.point2.GetCentralProjection(R);
+
+            var centerX = _screenCenter.X;
+            var centerY = _screenCenter.Y;
+
+            if (centerX + p1.X > float.MaxValue ||
+                centerX + p2.X > float.MaxValue ||
+                centerY - p1.Y < float.MinValue ||
+                centerY - p2.Y < float.MinValue)
+            {
+                continue;
+            }
+
+            _g.DrawLine(new Pen(Color.Black),
+                    centerX + p1.X, centerY - p1.Y,
+                    centerX + p2.X, centerY - p2.Y);
+        }
+
+        mainView.Image = (Bitmap)mainView.Image;
+    }
+
+    private void Translate(float Dx = 0, float Dy = 0, float Dz = 0)
+    {
+        _floor.Translate(Dx, Dy, Dz);
+    }
+
+    private void RotateAt(float angleDegree, int rotationAxis)
+    {
+        var floorCenterPoint = _floor.CenterPoint;
+
+        _floor.RotateAt(floorCenterPoint, angleDegree, rotationAxis);
+    }
+
+    private void MainViewClear()
+    {
+        mainView.Image = new Bitmap(mainView.Image.Width, mainView.Image.Height);
+        _g = Graphics.FromImage(mainView.Image);
+    }
+
+    private void MainView_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+    {
+        if (e.Shift)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                Translate(100);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                Translate(-100);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                Translate(0, 0, -100);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                Translate(0, 0, 100);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.Q)
+            {
+                Translate(0, 100);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.E)
+            {
+                Translate(0, -100);
+                DrawFloor();
+            }
+        }
+        else
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                Translate(5);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                Translate(-5);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                Translate(0, 0, -5);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                Translate(0, 0, 5);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.Q)
+            {
+                Translate(0, 5);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.E)
+            {
+                Translate(0, -5);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.I)
+            {
+                RotateAt(5, 0);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.K)
+            {
+                RotateAt(-5, 0);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.J)
+            {
+                RotateAt(5, 1);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.L)
+            {
+                RotateAt(-5, 1);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.U)
+            {
+                RotateAt(5, 2);
+                DrawFloor();
+            }
+            else if (e.KeyCode == Keys.O)
+            {
+                RotateAt(-5, 2);
+                DrawFloor();
+            }
+        }
+
+    }
 }
