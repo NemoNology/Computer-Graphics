1b319ced1e9f5e8db93344ff1bf1770be1413519
NemoNology <chicken.gang@mail.ru>
2023-05-02 03:08:46 +0900

    Started step 1.2 from plan

--- Lab-s/12/Project/MainForm.Designer.cs
+++ Lab-s/12/Project/MainForm.Designer.cs
@@ -28,11 +28,58 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        splitContainer1 = new SplitContainer();
+        outputView = new PictureBox();
+        ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+        splitContainer1.Panel1.SuspendLayout();
+        splitContainer1.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)outputView).BeginInit();
+        SuspendLayout();
+        // 
+        // splitContainer1
+        // 
+        splitContainer1.Dock = DockStyle.Fill;
+        splitContainer1.Location = new Point(0, 0);
+        splitContainer1.Name = "splitContainer1";
+        // 
+        // splitContainer1.Panel1
+        // 
+        splitContainer1.Panel1.Controls.Add(outputView);
+        // 
+        // splitContainer1.Panel2
+        // 
+        splitContainer1.Panel2.BackColor = SystemColors.ScrollBar;
+        splitContainer1.Size = new Size(800, 450);
+        splitContainer1.SplitterDistance = 590;
+        splitContainer1.SplitterWidth = 8;
+        splitContainer1.TabIndex = 0;
+        // 
+        // outputView
+        // 
+        outputView.Dock = DockStyle.Fill;
+        outputView.Location = new Point(0, 0);
+        outputView.Name = "outputView";
+        outputView.Size = new Size(590, 450);
+        outputView.TabIndex = 0;
+        outputView.TabStop = false;
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(splitContainer1);
+        Name = "MainForm";
+        Text = "Water";
+        splitContainer1.Panel1.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+        splitContainer1.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)outputView).EndInit();
+        ResumeLayout(false);
     }
 
     #endregion
+
+    private SplitContainer splitContainer1;
+    private PictureBox outputView;
 }
--- Lab-s/12/Project/MainForm.cs
+++ Lab-s/12/Project/MainForm.cs
@@ -1,3 +1,6 @@
+using GraphicUnit;
+using System.Data;
+
 namespace Project;
 
 public partial class MainForm : Form
@@ -5,5 +8,19 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+
+        _waterVoxels.Add(new Cube(new Point3D(), 20));
+
+        OutputView_Clear();
+    }
+
+    private List<Cube> _waterVoxels = new List<Cube>();
+
+    private Graphics _graphics;
+
+    private void OutputView_Clear()
+    {
+        outputView.Image = new Bitmap(outputView.Width, outputView.Height);
+        _graphics = Graphics.FromImage(outputView.Image);
     }
 }
