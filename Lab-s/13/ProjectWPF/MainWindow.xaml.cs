b67fc12bfcf80cabaf6edae1d784967a733f8d11
NemoNology <chicken.gang@mail.ru>
2023-05-07 22:20:54 +0900

    Added User simple Polygon input

--- Lab-s/13/ProjectWPF/MainWindow.xaml.cs
+++ Lab-s/13/ProjectWPF/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using Units;
 
 namespace ProjectWPF
 {
@@ -16,7 +16,7 @@ namespace ProjectWPF
             InitializeComponent();
         }
 
-        private void MainView_MouseDown(object sender, MouseButtonEventArgs e)
+        private async void MainView_MouseDown(object sender, MouseButtonEventArgs e)
         {
             var point = e.GetPosition(mainView);
             var pointsAmount = polygonLines.Points.Count;
@@ -31,9 +31,53 @@ namespace ProjectWPF
             {
                 polygonLines.Points.Add(point);
                 polygonLines.Points.Add(point);
+
+                return;
             }
 
-            polygonLines.Points.Insert(polygonLines.Points.Count - 2, point);
+            // Checking if this simple polygon
+            if (pointsAmount > 3)
+            {
+                var points = polygonLines.Points;
+
+                var newLinePoint1 = point;
+                var newLinePoint2 = 
+                    GeometryUnit.MoveSecondPointCloserToFirstPoint(
+                        newLinePoint1, 
+                        points[pointsAmount - 2], 20);
+
+                var lastLinePoint1 = point;
+                var lastLinePoint2 = points[0];
+
+                for (int i = 0; i < pointsAmount - 2; i++)
+                {
+                    var oldLinePoint1 = points[i];
+                    var oldLinePoint2 = points[i + 1];
+
+                    if (GeometryUnit.IsLinesIntersect(
+                        newLinePoint1,
+                        newLinePoint2,
+                        oldLinePoint1,
+                        oldLinePoint2)
+                        || 
+                        (i > 0
+                        &&
+                        GeometryUnit.IsLinesIntersect(
+                        lastLinePoint1,
+                        lastLinePoint2,
+                        oldLinePoint1,
+                        oldLinePoint2)))
+                    {
+                        polygonLines.Fill = Brushes.Red;
+                        await System.Threading.Tasks.Task.Delay(200);
+                        polygonLines.Fill = Brushes.LightGray;
+
+                        return;
+                    }
+                }
+            }
+
+            polygonLines.Points.Insert(polygonLines.Points.Count - 1, point);
         }
 
     }
