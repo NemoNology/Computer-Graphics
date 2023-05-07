de5a67fa87fa2c900e8eb8e91b55deec5d555b5f
NemoNology <chicken.gang@mail.ru>
2023-05-08 00:49:07 +0900

    Improved? polygon simplicity check

--- Lab-s/13/ProjectWPF/MainWindow.xaml.cs
+++ Lab-s/13/ProjectWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using Units;
 
 namespace ProjectWPF
@@ -21,12 +22,92 @@ namespace ProjectWPF
             var point = e.GetPosition(mainView);
             var pointsAmount = polygonLines.Points.Count;
 
-            if (e.RightButton == MouseButtonState.Pressed)
+            // Points clearing
+            if (e.MiddleButton == MouseButtonState.Pressed)
             {
                 polygonLines.Points.Clear();
                 return;
             }
+            // Polygon drawing
+            else if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                PolygonPointsAdding(pointsAmount, point);
+            }
+            // Polygon triangulation
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                PolygonTriangulation();
+            }
+            
+        }
 
+        private void PolygonTriangulation()
+        {
+            var pointsAmount = polygonLines.Points.Count;
+
+            if (pointsAmount == 0)
+            {
+                MessageBox.Show("Polygon is not drawn");
+                return;
+            }
+
+            // Checking if this polygon is simple
+            if (pointsAmount > 3)
+            {
+                var points = polygonLines.Points;
+
+                var lastLinePoint1 = points.Last();
+                var lastLinePoint2 = points[0];
+
+                // Checking last line intersection
+                for (int i = 1; i < pointsAmount - 2; i++)
+                {
+                    var oldLinePoint1 = points[i];
+                    var oldLinePoint2 = points[i + 1];
+
+                    if (GeometryUnit.IsLinesIntersect(
+                        lastLinePoint1,
+                        lastLinePoint2,
+                        oldLinePoint1,
+                        oldLinePoint2))
+                    {
+                        MessageBox.Show("Polygon is not simple");
+                        return;
+                    }
+                }
+
+                for (int i = 0; i < pointsAmount - 2; i++)
+                {
+                    var currentLinePoint1 = points[i];
+                    var currentLinePoint2 = 
+                        GeometryUnit.MoveSecondPointCloserToFirstPoint(
+                            currentLinePoint1,
+                            points[i + 1]);
+
+                    for (int j = i + 1; j < pointsAmount - 2; j++)
+                    {
+                        var lineBufferPoint1 = points[j];
+                        var lineBufferPoint2 = points[j + 1];
+
+                        if (GeometryUnit.IsLinesIntersect(
+                        currentLinePoint1,
+                        currentLinePoint2,
+                        lineBufferPoint1,
+                        lineBufferPoint2))
+                        {
+                            MessageBox.Show("Polygon is not simple");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            // TODO: Triangulation
+            MessageBox.Show("Polygon is simple");
+        }
+
+        private void PolygonPointsAdding(int pointsAmount, Point point)
+        {
             if (pointsAmount == 0)
             {
                 polygonLines.Points.Add(point);
@@ -35,50 +116,7 @@ namespace ProjectWPF
                 return;
             }
 
-            // Checking if this simple polygon
-            if (pointsAmount > 3)
-            {
-                var points = polygonLines.Points;
-
-                var newLinePoint1 = point;
-                var newLinePoint2 = 
-                    GeometryUnit.MoveSecondPointCloserToFirstPoint(
-                        newLinePoint1, 
-                        points[pointsAmount - 2], 20);
-
-                var lastLinePoint1 = point;
-                var lastLinePoint2 = points[0];
-
-                for (int i = 0; i < pointsAmount - 2; i++)
-                {
-                    var oldLinePoint1 = points[i];
-                    var oldLinePoint2 = points[i + 1];
-
-                    if (GeometryUnit.IsLinesIntersect(
-                        newLinePoint1,
-                        newLinePoint2,
-                        oldLinePoint1,
-                        oldLinePoint2)
-                        || 
-                        (i > 0
-                        &&
-                        GeometryUnit.IsLinesIntersect(
-                        lastLinePoint1,
-                        lastLinePoint2,
-                        oldLinePoint1,
-                        oldLinePoint2)))
-                    {
-                        polygonLines.Fill = Brushes.Red;
-                        await System.Threading.Tasks.Task.Delay(200);
-                        polygonLines.Fill = Brushes.LightGray;
-
-                        return;
-                    }
-                }
-            }
-
-            polygonLines.Points.Insert(polygonLines.Points.Count - 1, point);
+            polygonLines.Points.Insert(pointsAmount - 1, point);
         }
-
     }
 }
